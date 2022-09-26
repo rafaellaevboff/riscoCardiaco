3156dad37bb670b702be2a370b9189c75e7beddc
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-09-26 20:15:05 -0300

    Criando intents e adicionando no Android Manifest + refatoração tela final + refatoração nomes dos botões

--- app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/FinalActivity.kt
+++ app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/FinalActivity.kt
@@ -3,18 +3,17 @@ package com.rafaebofflarissacarlos.riscocardiaco.intents
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.rafaebofflarissacarlos.riscocardiaco.R
 
-class IdadeActivity : AppCompatActivity() {
+class FinalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_idade)
+        setContentView(R.layout.activity_final)
 
-        var button = findViewById<Button>(R.id.button)
+        var button = findViewById<Button>(R.id.buttoninicio)
         button.setOnClickListener{
-            val intent = Intent(applicationContext, GeneroActivity::class.java)
+            val intent = Intent(applicationContext, MainActivity::class.java)
 
             startActivity(intent)
         }
--- app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/FumanteActivity.kt
+++ app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/FumanteActivity.kt
@@ -3,18 +3,17 @@ package com.rafaebofflarissacarlos.riscocardiaco.intents
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.rafaebofflarissacarlos.riscocardiaco.R
 
-class IdadeActivity : AppCompatActivity() {
+class FumanteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_idade)
+        setContentView(R.layout.activity_fumante)
 
         var button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
-            val intent = Intent(applicationContext, GeneroActivity::class.java)
+            val intent = Intent(applicationContext, PressaoActivity::class.java)
 
             startActivity(intent)
         }
