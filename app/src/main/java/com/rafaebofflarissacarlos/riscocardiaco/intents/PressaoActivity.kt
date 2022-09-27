1cdfd43ffc4919126cb7c0f78fa49b2b7691f431
LarissaCarlos <mpkavolt.l@rede.ulbra.br>
2022-09-27 00:02:11 -0300

    enviando pressao

--- app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/PressaoActivity.kt
+++ app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/PressaoActivity.kt
@@ -3,19 +3,40 @@ package com.rafaebofflarissacarlos.riscocardiaco.intents
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rafaebofflarissacarlos.riscocardiaco.R
+import com.rafaebofflarissacarlos.riscocardiaco.databinding.ActivityPressaoBinding
 
-class PressaoActivity : AppCompatActivity() {
+class PressaoActivity   : AppCompatActivity() {
+    private lateinit var binding : ActivityPressaoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pressao)
+        binding = ActivityPressaoBinding.inflate(layoutInflater)
 
-        var button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            val intent = Intent(applicationContext, DoencaFamiliaActivity::class.java)
+        var resultado: Int = 0
 
-            startActivity(intent)
+        binding.button.setOnClickListener {
+            var idRadio: Int = binding.radioGroupPressao.checkedRadioButtonId
+
+            when(idRadio){
+                R.id.radioButton1-> resultado = 0
+                R.id.radioButton2-> resultado = 1
+                R.id.radioButton3-> resultado = 2
+                R.id.radioButton4-> resultado = 3
+                R.id.radioButton5-> resultado = 5
+                R.id.radioButton6-> resultado = 7
+                else-> Toast.makeText(applicationContext, "É preciso selecionar uma opção.", Toast.LENGTH_LONG).show()
+            }
+
+            val proximaPagina = Intent(this, DoencaFamiliaActivity::class.java)
+            startActivity(proximaPagina)
+
+            val enviarDados = Intent(this, PressaoActivity::class.java)
+            enviarDados.putExtra("PressaoResultado", resultado)
+            startActivity(proximaPagina)
         }
     }
 }
