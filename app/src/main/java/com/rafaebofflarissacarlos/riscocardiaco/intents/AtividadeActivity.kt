c9ffcb28c6b66cc7f458707d4ff6c1bc079aa33e
LarissaCarlos <mpkavolt.l@rede.ulbra.br>
2022-09-27 00:03:10 -0300

    enviando ativ

--- app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/AtividadeActivity.kt
+++ app/src/main/java/com/rafaebofflarissacarlos/riscocardiaco/intents/AtividadeActivity.kt
@@ -3,19 +3,40 @@ package com.rafaebofflarissacarlos.riscocardiaco.intents
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rafaebofflarissacarlos.riscocardiaco.R
+import com.rafaebofflarissacarlos.riscocardiaco.databinding.ActivityAtividadeBinding
 
 class AtividadeActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityAtividadeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_atividade)
+        binding = ActivityAtividadeBinding.inflate(layoutInflater)
 
-        var button = findViewById<Button>(R.id.button)
-        button.setOnClickListener{
-            val intent = Intent(applicationContext, FumanteActivity::class.java)
+        var resultado: Int = 0
 
-            startActivity(intent)
+        binding.button.setOnClickListener {
+            var idRadio: Int = binding.radioGroupAtividade.checkedRadioButtonId
+
+            when(idRadio){
+                R.id.radioButton1-> resultado = 0
+                R.id.radioButton2-> resultado = 1
+                R.id.radioButton3-> resultado = 2
+                R.id.radioButton4-> resultado = 3
+                R.id.radioButton5-> resultado = 5
+                R.id.radioButton6-> resultado = 7
+                else-> Toast.makeText(applicationContext, "É preciso selecionar uma opção.", Toast.LENGTH_LONG).show()
+            }
+
+            val proximaPagina = Intent(this, FumanteActivity::class.java)
+            startActivity(proximaPagina)
+
+            val enviarDados = Intent(this, AtividadeActivity::class.java)
+            enviarDados.putExtra("AtividadeResultado", resultado)
+            startActivity(proximaPagina)
         }
     }
 }
